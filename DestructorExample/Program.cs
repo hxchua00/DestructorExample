6b438efbbd40548e12dbeadea6ca23f4ca84ce22
hxchua00 <hxchua00@gmail.com>
2021-09-18 20:57:17 +0800

    Add project files.

---
+++ DestructorExample/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DestructorExample
+{
+    class A
+    { 
+        public A() { }
+        public void doSomething()
+        {
+            Console.WriteLine("I'm in the constructor!");
+        }
+
+        ~A()
+        {
+            Console.WriteLine("I'm in the destructor! Exiting now....");
+            Console.ReadLine();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A a = new A();
+
+            a.doSomething();
+            Console.ReadLine();
+
+            Environment.Exit(0);
+
+
+        }
+    }
+}
